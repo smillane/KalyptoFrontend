6e33e89c95aed7f740f56764c398fbbc6ac66272
Sean Millane <jmillaneiv@gmail.com>
2022-03-20 10:26:48 -0400

    changed db connection for dev env, add save to db logic, comment out some lines for testing

--- src/pages/stocks/[stock].js
+++ src/pages/stocks/[stock].js
@@ -1,40 +1,41 @@
-import { useRouter } from "next/router";
+import { useRouter } from "next/router"
 
-import { stockRelationships } from '../../main/node/relationships'
+import dbConnect from '../../main/node/database/dBConnect'
+import stockRelationships from '../../main/node/relationships'
 import { updateAndReplace, updateOnIntervalsAndAdd, findAndReturn, queryExistsCheck } from '../../main/node/queriesAndUpdates'
 import { stockQuoteModel, stockStatsBasicModel, stockLargestTradesModel, stockInsiderTradingModel, stockPreviousDividendsModel, stockNextDividendsModel } from '../../main/node/database/models/models'
 
 // if user is not logged in with an account, only show a basic quote and chart
-export default function Stock() {
-
-    const router = useRouter()
-    const { stock } = router.query
-
-    return (
-      <div>
-        <h1>{stock}</h1>
-        <h3>{response}</h3>
-      </div>
-    )
+export default function Stock({apiData}) {
+  const router = useRouter();
+  console.log(apiData);
+  
+  return (
+    <div>
+      <h1>{apiData}</h1>
+    </div>
+  )
 }
 
-export async function getServerSideProps(params) {
-  const updateAndReplaceQueries = new Map([[stockQuoteModel, [true, false]], [stockStatsBasicModel, [false, false]], [stockLargestTradesModel, [false, false]], [stockInsiderTradingModel, [false, true]]]);
-  const updateOnIntervalsAndAddQueries = [stockNextDividendsModel];
-  const findAndReturnQueries = [stockPreviousDividendsModel];
+export async function getServerSideProps(context) {
+  await dbConnect();
 
-  if (!queryExistsCheck(params.id)) {
+  const stock = context.params.stock;
+  const apiData = queryExistsCheck(stock);
+  // const updateAndReplaceQueries = new Map([[stockQuoteModel, [true, false]], [stockStatsBasicModel, [false, false]], [stockLargestTradesModel, [false, false]], [stockInsiderTradingModel, [false, true]]]);
+  // const updateOnIntervalsAndAddQueries = [stockNextDividendsModel];
+  // const findAndReturnQueries = [stockPreviousDividendsModel];
+
+  if (!apiData) {
     return {
       notFound: true
     }
   }
 
-  const response = updateAndReplaceQueries.forEach((value, key) => {updateAndReplace(params.id, stockRelationships.get(key) ,key, value[0], value[1])});
-  // const response2 = updateOnIntervalsAndAdd(params.id, stockRelationships.get(stockNextDividendsModel), stockNextDividendsModel, stockPreviousDividendsModel);
-  // const response3 = findAndReturn(params.id, stockRelationships.get(stockPreviousDividendsModel), stockPreviousDividendsModel);
+  //const response = updateAndReplaceQueries.forEach((value, key) => {updateAndReplace(stock, stockRelationships.get(key) ,key, value[0], value[1])});
+  // const response2 = updateOnIntervalsAndAdd(stock, stockRelationships.get(stockNextDividendsModel), stockNextDividendsModel, stockPreviousDividendsModel);
+  // const response3 = findAndReturn(stock, stockRelationships.get(stockPreviousDividendsModel), stockPreviousDividendsModel);
 
-  return {
-    props: {response},
-  }
+  return { props: {apiData} }
 }
   
