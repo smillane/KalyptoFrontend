aa0f4272a6a731e7d0242b2d5455f4e9c6b66576
Sean Millane <jmillaneiv@gmail.com>
2022-08-10 03:01:35 -0400

    adding redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { SessionProvider } from "next-auth/react"
+import { Provider } from 'react-redux'
+
+import { store } from '../main/node/redux/store'
 import "../styles/styles.css"
+
 
 export default function App({ Component, pageProps: { session, ...pageProps }, }) {  
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Provider store={store}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </Provider>
   );
 }
