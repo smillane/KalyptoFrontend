e8d950a9ec9abd6fbe4e23cca47e53779de9e369
Sean Millane <jmillaneiv@gmail.com>
2022-08-11 22:41:34 -0400

    adjustments for redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { SessionProvider } from "next-auth/react"
-import { Provider } from 'react-redux'
+import React from 'react';
+import { SessionProvider } from "next-auth/react";
+import { Provider } from 'react-redux';
 
-import { store } from '../main/node/redux/store'
-import "../styles/styles.css"
+import { store } from '../main/node/redux/store';
+import "../styles/styles.css";
 
 
 export default function App({ Component, pageProps: { session, ...pageProps }, }) {  
   return (
-    <Provider store={store}>
-      <SessionProvider session={session}>
+    <SessionProvider session={session}>
+      <Provider store={store}>
         <Component {...pageProps} />
-      </SessionProvider>
-    </Provider>
+      </Provider>
+    </SessionProvider>
   );
 }
