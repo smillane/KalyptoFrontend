a7f76ce0dd27d99f5f418d74d3b0404cc83014ba
Sean Millane <jmillaneiv@gmail.com>
2022-09-25 01:40:16 -0400

    firebase auth integration

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,34 +3,28 @@ import {
   withAuthUser,
   withAuthUserTokenSSR,
 } from 'next-firebase-auth';
+import { Container, Grid, Text } from '@mantine/core';
 
 import Layout from '../main/node/components/layout.tsx';
-
-const styles = {
-  content: {
-    padding: 32,
-  },
-  infoTextContainer: {
-    marginBottom: 32,
-  },
-};
+import Watchlist from '../main/node/redux/features/userLists/Watchlist.tsx';
 
 function Demo() {
   return (
     <Layout>
-      <div style={styles.content}>
-        <div style={styles.infoTextContainer}>
+      <Grid grow>
+        <Container size="xl">
           <h3>Home</h3>
-          <p>
-            This page does not require authentication, so it won't redirect to
+          <Text>
+            This page does not require authentication, so it won&apos;t redirect to
             the login page if you are not signed in.
-          </p>
-          <p>
+          </Text>
+          <Text>
             If you remove `getServerSideProps` from this page, it will be static
             and load the authed user only on the client side.
-          </p>
-        </div>
-      </div>
+          </Text>
+        </Container>
+        <Watchlist />
+      </Grid>
     </Layout>
   );
 }
